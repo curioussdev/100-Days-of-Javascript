7a8290e5bab505f564137d2fddab0c05d78124ae
Jose Lopes <946393361bill@gmail.com>
2021-09-02 23:22:26 +0100

    the other way to develop a counter app

--- Day 1 - Counter App/app.js
+++ Day 1 - Counter App/app.js
@@ -1,9 +1,10 @@
-const count = document.querySelector(".count");
-const add = document.querySelector(".add");
-const resetCount = document.querySelector(".reset");
-const sub = document.querySelector(".subtract");
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const sub = document.querySelector(".subtract");
 
-add.addEventListener("click", () =>{
+
+
+/*add.addEventListener("click", () =>{
     count.innerHTML++;
 });
 sub.addEventListener("click", () =>{
@@ -11,4 +12,4 @@ sub.addEventListener("click", () =>{
 })
 resetCount.addEventListener("click", () =>{
     count.innerHTML = 0;
-})
+})*/
