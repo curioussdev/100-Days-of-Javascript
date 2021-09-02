d73bd7d497e83c59fbea0d1ce3a7fe4c39371fe7
Jose Lopes <946393361bill@gmail.com>
2021-09-02 21:41:15 +0100

    creating all const

---
+++ Day 1 - Counter App/app.js
@@ -0,0 +1,5 @@
+const count = document.querySelector('.count');
+const add = document.querySelector('.add');
+const reset = document.querySelector('.reset');
+const sub = document.querySelector('.sub');
+
