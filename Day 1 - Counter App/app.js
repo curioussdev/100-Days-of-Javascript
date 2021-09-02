9fc6e77e16689c4ee06a47178e4b40453b12bf49
Jose Lopes <946393361bill@gmail.com>
2021-09-02 23:16:55 +0100

    solving script atag position error

--- Day 1 - Counter App/app.js
+++ Day 1 - Counter App/app.js
@@ -1,5 +1,14 @@
-const count = document.querySelector('.count');
-const add = document.querySelector('.add');
-const reset = document.querySelector('.reset');
-const sub = document.querySelector('.sub');
+const count = document.querySelector(".count");
+const add = document.querySelector(".add");
+const resetCount = document.querySelector(".reset");
+const sub = document.querySelector(".subtract");
 
+add.addEventListener("click", () =>{
+    count.innerHTML++;
+});
+sub.addEventListener("click", () =>{
+    count.innerHTML--;
+})
+resetCount.addEventListener("click", () =>{
+    count.innerHTML = 0;
+})
