eafbee10da8bb8c4ea3c5d1bf88d25b57b5a19a4
Jose Lopes <946393361bill@gmail.com>
2021-09-02 23:49:47 +0100

    turning the code into a function

--- Day 1 - Counter App/app.js
+++ Day 1 - Counter App/app.js
@@ -5,18 +5,21 @@
 const count = document.querySelector(".count");
 const buttons = document.querySelector(".buttons");
 
-buttons.addEventListener("click", (e) => {
-    if(e.target.classList.contains("add")) {
-        count.innerHTML++;
-    }
-    if(e.target.classList.contains("subtract")) {
-        count.innerHTML--;
-    }
-    if(e.target.classList.contains("reset")) {
-        count.innerHTML = 0;
-    }
-})
+function TheCounter() {
+    buttons.addEventListener("click", (e) => {
+        if(e.target.classList.contains("add")) {
+            count.innerHTML++;
+        }
+        if(e.target.classList.contains("subtract")) {
+            count.innerHTML--;
+        }
+        if(e.target.classList.contains("reset")) {
+            count.innerHTML = 0;
+        }
+    })
+}
 
+TheCounter();
 /*add.addEventListener("click", () =>{
     count.innerHTML++;
 });
