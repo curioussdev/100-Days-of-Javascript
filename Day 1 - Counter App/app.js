20d2a191d4f1491da5dc99c63c02ff01a61e2586
Jose Lopes <946393361bill@gmail.com>
2021-09-02 23:37:19 +0100

    complete and functional

--- Day 1 - Counter App/app.js
+++ Day 1 - Counter App/app.js
@@ -2,7 +2,20 @@
 // const resetCount = document.querySelector(".reset");
 // const sub = document.querySelector(".subtract");
 
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
 
+buttons.addEventListener("click", (e) => {
+    if(e.target.classList.contains("add")) {
+        count.innerHTML++;
+    }
+    if(e.target.classList.contains("subtract")) {
+        count.innerHTML--;
+    }
+    if(e.target.classList.contains("reset")) {
+        count.innerHTML = 0;
+    }
+})
 
 /*add.addEventListener("click", () =>{
     count.innerHTML++;
