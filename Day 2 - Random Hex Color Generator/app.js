6da54aae265e3e68c34c51625bd03d1fec9495b3
Jose Lopes <946393361bill@gmail.com>
2021-09-03 22:22:18 +0100

    finish

--- Day 2 - Random Hex Color Generator/app.js
+++ Day 2 - Random Hex Color Generator/app.js
@@ -1,1 +1,13 @@
-const hex = documen.querySelector(".hex")
+const hex = document.querySelector(".hex");
+const btn = document.querySelector(".generate");
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring
+    (2, 8);
+    document.body.style.backgroundColor = "#" + randomColor;
+    hex.innerHTML = "#" + randomColor;
+};
+
+btn.addEventListener("click", generateColor);
+
+generateColor()
