47b0b1e4aabba003dbd735d83bf52c05fdd0434a
Jose Lopes <946393361bill@gmail.com>
2021-09-03 01:46:45 +0100

    applying color change function setting the counter

--- Day 1 - Counter App/app.js
+++ Day 1 - Counter App/app.js
@@ -11,17 +11,31 @@ function TheCounter() {
     buttons.addEventListener("click", (e) => {
         if(e.target.classList.contains("add")) {
             count.innerHTML++;
+            setColor()
         }
         if(e.target.classList.contains("subtract")) {
             count.innerHTML--;
+            setColor();
         }
         if(e.target.classList.contains("reset")) {
             count.innerHTML = 0;
+            setColor();
         }
     })
 }
 
+function setColor() {
+    if(count.innerHTML > 0) {
+        count.style.color = "#1C6A09"
+    } else if(count.innerHTML < 0) {
+        count.style.color = "#EF5A3A"
+    } else {
+        count.style.color = "#000"
+    }
+}
+
 TheCounter();
+setColor();
 /*add.addEventListener("click", () =>{
     count.innerHTML++;
 });
